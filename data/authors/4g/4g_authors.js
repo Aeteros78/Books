2f3814ca6caca2aa6702632b82f9c481da7e9e9d
Aeteros78 <Myzikant161@yandex.ru>
2026-07-19 22:42:54 +0300

    + g

---
+++ data/authors/4g/4g_authors.js
@@ -0,0 +1,89 @@
+// data/authors/4g/4g_authors.js
+
+// 1. Импорты всех авторов из папки 4g
+import { gerritsen_tess } from './gerritsen_tess.js';
+import { hesse_hermann } from './hesse_hermann.js';
+import { goethe_johann } from './goethe_johann.js';
+import { gibbon_lewis } from './gibbon_lewis.js';
+import { gibbons_stella } from './gibbons_stella.js';
+import { gilbert_elizabeth } from './gilbert_elizabeth.js';
+import { gladwell_malcolm } from './gladwell_malcolm.js';
+import { gogol_nikolai } from './gogol_nikolai.js';
+import { godin_set } from './godin_set.js';
+import { golden_arthur } from './golden_arthur.js';
+import { golding_william } from './golding_william.js';
+import { golovkina_irina } from './golovkina_irina.js';
+import { golon_serge } from './golon_serge.js';
+import { galsworthy_john } from './galsworthy_john.js';
+import { homer } from './homer.js';
+import { goncharov_ivan } from './goncharov_ivan.js';
+import { horace } from './horace.js';
+import { gordner_joestein } from './gordner_joestein.js';
+import { goulman_daniel } from './goulman_daniel.js';
+import { grabs_west_lorraine } from './grabs_west_lorraine.js';
+import { granin_daniil } from './granin_daniil.js';
+import { grey_muriel } from './grey_muriel.js';
+import { griboyedov_alexandr } from './griboyedov_alexandr.js';
+import { grimaldi_virginie } from './grimaldi_virginie.js';
+import { grin_alexander } from './grin_alexander.js';
+import { grin_graham } from './grin_graham.js';
+import { grin_john } from './grin_john.js';
+import { grin_robert } from './grin_robert.js';
+import { gram_kenneth } from './gram_kenneth.js';
+import { mitchell_mitchell } from './mitchell_mitchell.js';
+import { grossmith_george } from './grossmith_george.js';
+import { grof_stanislav } from './grof_stanislav.js';
+import { groff_lauren } from './groff_lauren.js';
+import { gruen_sarah } from './gruen_sarah.js';
+import { goodkind_terry } from './goodkind_terry.js';
+import { le_guin_ursula } from './le_guin_ursula.js';
+import { gabaldon_diana } from './gabaldon_diana.js';
+import { galbraith_robert } from './galbraith_robert.js';
+import { hugo_victor } from './hugo_victor.js';
+import { herbert_dune_universe } from './herbert_dune_universe.js';
+
+// 2. Сборка всех в один объект по ключу "г"
+export const authors_4g = {
+    "г": {
+        "Герритсен Тесс": gerritsen_tess.cycles,
+        "Гессе Герман": hesse_hermann.cycles,
+        "Гете Иоганн": goethe_johann.cycles,
+        "Гиббон Льюис Грессик": gibbon_lewis.cycles,
+        "Гиббонс Стелла": gibbons_stella.cycles,
+        "Гилберт Элизабет": gilbert_elizabeth.cycles,
+        "Гладуэлл Мальком": gladwell_malcolm.cycles,
+        "Гоголь Николай": gogol_nikolai.cycles,
+        "Годин Сет": godin_set.cycles,
+        "Голден Артур": golden_arthur.cycles,
+        "Голдинг Уильям": golding_william.cycles,
+        "Головкина Ирина": golovkina_irina.cycles,
+        "Голон Серж": golon_serge.cycles,
+        "Голсуори Джон": galsworthy_john.cycles,
+        "Гомер": homer.cycles,
+        "Гончаров Иван": goncharov_ivan.cycles,
+        "Гораций": horace.cycles,
+        "Гордер Йостейн": gordner_joestein.cycles,
+        "Гоулман Дениэл": goulman_daniel.cycles,
+        "Грабс-Уэст Лорейн": grabs_west_lorraine.cycles,
+        "Гранин Даниил": granin_daniil.cycles,
+        "Грей Мьюриел": grey_muriel.cycles,
+        "Грибоедов Александр": griboyedov_alexandr.cycles,
+        "Гримальди Виржини": grimaldi_virginie.cycles,
+        "Грин Александр": grin_alexander.cycles,
+        "Грин Грэм": grin_graham.cycles,
+        "Грин Джон": grin_john.cycles,
+        "Грин Роберт": grin_robert.cycles,
+        "Грэм Кеннет": gram_kenneth.cycles,
+        "Митчелл Митчелл": mitchell_mitchell.cycles,
+        "Гроссмит Джордж": grossmith_george.cycles,
+        "Гроф Станислав": grof_stanislav.cycles,
+        "Грофф Лорен": groff_lauren.cycles,
+        "Груэн Сара": gruen_sarah.cycles,
+        "Гудкаинд Тэрри": goodkind_terry.cycles,
+        "Ле Гуин Урсула": le_guin_ursula.cycles,
+        "Гэблдон Диана": gabaldon_diana.cycles,
+        "Гэлбрейт Роберт": galbraith_robert.cycles,
+        "Гюго Виктор": hugo_victor.cycles,
+        "Герберт": herbert_dune_universe.cycles
+    }
+};
